9d4d749dfa5ca4a2cb4ed1435f3fd2e3e3489e6f
chai2010 <chaishushan@gmail.com>
2015-06-05 17:35:56 +0800

    简化默认驱动名的查找

--- driver_name.go
+++ driver_name.go
@@ -10,53 +10,48 @@ import (
 )
 
 func getDriverName(filename string) string {
-	filename = strings.ToLower(filename)
-	if strings.HasSuffix(filename, "toc.xml") {
-		return "ECRGTOC"
-	}
-	ext := filepath.Ext(filename)
+	ext := strings.ToLower(filepath.Ext(filename))
 	s, _ := defaultDriverNameMap[ext]
 	return s
 }
 
 // See http://www.gdal.org/formats_list.html
 var defaultDriverNameMap = map[string]string{
-	".blx":    "BLX",
-	".xlb":    "BLX",
-	".bmp":    "BMP",
-	".kap":    "BSB",
-	".bt":     "BT",
-	".dim":    "DIMAP",
-	".dog":    "DOQ1", // "DOQ2",
-	".dt0":    "DTED",
-	".dt1":    "DTED",
-	".dt2":    "DTED",
-	"toc.xml": "ECRGTOC", // /path/to/TOC.xml
-	".hdr":    "EHdr",    // "ENVI","GENBIN"
-	".ers":    "ERS",
-	".n1":     "ESAT",
-	".gif":    "GIF",
-	".grb":    "GRIB",
-	".gta":    "GTA",
-	".tif":    "GTiff",
-	".tiff":   "GTiff",
-	".img":    "HFA",
-	".mpr":    "ILWIS",
-	".mpl":    "ILWIS",
-	".jpg":    "JPEG",
-	".jpeg":   "JPEG",
-	".ntf":    "NITF",
-	".nsf":    "NITF",
-	".grc":    "NWT_GRC",
-	".tab":    "NWT_GRC",
-	".png":    "PNG",
-	".ppm":    "PNM",
-	".pgm":    "PNM",
-	".rik":    "RIK",
-	".rsw":    "RMF",
-	".mtw":    "RMF",
-	".ter":    "TERRAGEN",
-	".dem":    "USGSDEM",
-	".vrt":    "VRT",
-	".xpm":    "XPM",
+	".blx":  "BLX",
+	".xlb":  "BLX",
+	".bmp":  "BMP",
+	".kap":  "BSB",
+	".bt":   "BT",
+	".dim":  "DIMAP",
+	".dog":  "DOQ1", // "DOQ2",
+	".dt0":  "DTED",
+	".dt1":  "DTED",
+	".dt2":  "DTED",
+	".hdr":  "EHdr", // "ENVI","GENBIN"
+	".ers":  "ERS",
+	".n1":   "ESAT",
+	".gif":  "GIF",
+	".grb":  "GRIB",
+	".gta":  "GTA",
+	".tif":  "GTiff",
+	".tiff": "GTiff",
+	".img":  "HFA",
+	".mpr":  "ILWIS",
+	".mpl":  "ILWIS",
+	".jpg":  "JPEG",
+	".jpeg": "JPEG",
+	".ntf":  "NITF",
+	".nsf":  "NITF",
+	".grc":  "NWT_GRC",
+	".tab":  "NWT_GRC",
+	".png":  "PNG",
+	".ppm":  "PNM",
+	".pgm":  "PNM",
+	".rik":  "RIK",
+	".rsw":  "RMF",
+	".mtw":  "RMF",
+	".ter":  "TERRAGEN",
+	".dem":  "USGSDEM",
+	".vrt":  "VRT",
+	".xpm":  "XPM",
 }
